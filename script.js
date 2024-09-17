da70565139f1b34e397603b563ff563bd2ccedec
Andressa <andressacaroline082011@gmail.com>
2024-09-17 15:35:01 -0300

    Projeto Finalizado

--- script.js
+++ script.js
@@ -198,7 +198,7 @@ checkoutBtn.addEventListener("click", function () {
 function checkRestaurantOpen() {
     const data = new Date();
     const hora = data.getHours();
-    return hora >= 12 && hora < 22;
+    return hora >= 18 && hora < 22;
     //true = restaurante está aberto 
 }
 
