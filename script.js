313ca058a76b4d141139c2a9ca307469e66360dc
Andressa <andressacaroline082011@gmail.com>
2024-09-17 18:06:40 -0300

    Arrumando

--- script.js
+++ script.js
@@ -185,7 +185,7 @@ checkoutBtn.addEventListener("click", function () {
     }).join("")
 
     const message = encodeURIComponent(cartItems)
-    const phone = "37999784909"
+    const phone = "+5537999784909"
 
     window.open(`https://wa.me/${phone}?text=${message} Endereço: ${addressInput.value}`, "_blank")
 
